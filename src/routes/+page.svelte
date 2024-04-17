d7b729ae17b076f8a245fab0c234e8c6ee67016b
Dmitry Paliy <d@paliy.me>
2024-04-18 00:10:06 +0300

    reproduction based on create-svelte-app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { loadStripe, type Stripe } from "@stripe/stripe-js";
+  import { Elements } from "svelte-stripe";
+  import { onMount } from "svelte";
+
+  let stripe: Stripe | null;
+
+  onMount(async () => {
+    stripe = await loadStripe("abc");
+  });
+</script>
+
+{#if stripe}
+  <Elements {stripe}>
+    <!-- ... -->
+  </Elements>
+{/if}
